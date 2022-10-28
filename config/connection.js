147237d16063020c4c07b9c83f1bf697e03afe35
Michael Correa <sales@micomar.com>
2022-10-28 03:06:20 -0400

    api created

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialmedia', {
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/social_media', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
